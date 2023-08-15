4c4f6760f04f8904eb599432f12a8b158250cddf
talhade <talhademirkn@gmail.com>
2023-08-15 11:35:19 +0300

    Word Project Initial Commit

---
+++ wordProject/wordProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wordProject
+//
+//  Created by Talha Demirkan on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
