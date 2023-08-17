abf7036b56f73d2c12cb17b53e6ae722a72e8e96
talhade <talhademirkn@gmail.com>
2023-08-17 13:26:29 +0300

    GCD

---
+++ petitionsProjectGCD/petitionsProject/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  petitionsProject
+//
+//  Created by Talha Demirkan on 16.08.2023.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition ]
+}
